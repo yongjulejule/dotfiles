8d3b7420151a929f618212bfa5810a88c04be334
yongjulejule <lyjshow200@gmail.com>
2023-12-22 14:39:25 +0900

    feat: add dotfiles

---
+++ README.md
@@ -0,0 +1,13 @@
+# zsh
+
+$HOME/.zshrc
+$HOME/.p10k.zsh
+
+# neovim
+
+$HOME/.config/nvim/
+${XDG_CONFIG_HOME:-$HOME/.config}/nvim
+
+tree sitter wgsl 추가 설치 필요 https://github.com/szebniok/tree-sitter-wgsl
+
+
---
+++ nvim/lua/core/plugins.lua
@@ -0,0 +1,24 @@
+require("lazy").setup({
+  { "catppuccin/nvim", name = "mocha", priority = 1000 },
+  { "numToStr/Comment.nvim", lazy = false },
+  "nvim-tree/nvim-tree.lua",
+  "nvim-tree/nvim-web-devicons",
+  "nvim-lualine/lualine.nvim",
+  "nvim-treesitter/nvim-treesitter",
+  "lewis6991/gitsigns.nvim",
+  "tpope/vim-fugitive",
+  "tpope/vim-surround",
+  "stevearc/oil.nvim",
+  "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-nvim-lsp",
+  "L3MON4D3/LuaSnip",
+  "saadparwaiz1/cmp_luasnip",
+  "github/copilot.vim",
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "neovim/nvim-lspconfig",
+  {
+    "nvim-telescope/telescope.nvim", tag = "0.1.4",
+    dependencies = { "nvim-lua/plenary.nvim" }
+  },
+})
