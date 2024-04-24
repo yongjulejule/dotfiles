b640bb374ed423e6266eb9742b9f291521fb3419
yongjulejule <lyjshow200@gmail.com>
2024-04-24 16:22:35 +0900

    feat: delete legacy

--- legacy/nvim-20231227155706/lua/core/keymaps.lua
+++
@@ -1,6 +0,0 @@
--- Navigate vim panes better
-vim.keymap.set('n', '<c-h>', ':bp<CR>')
-vim.keymap.set('n', '<c-l>', ':bn<CR>')
-vim.keymap.set('n', '<c-q>', ':bd<CR>')
-
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
