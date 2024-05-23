abb16150e38099a6de1fc3970f49faf70c88d6bd
yongjulejule <lyjshow200@gmail.com>
2024-05-23 09:17:52 +0900

    feat: add description for telescope

--- nvim/lua/core/plugin_config/telescope.lua
+++ nvim/lua/core/plugin_config/telescope.lua
@@ -18,12 +18,12 @@ require('telescope').setup({
 local builtin = require('telescope.builtin')
 
 -- telescope official keymaps
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Help tags' })
 
 -- custom keymaps
-vim.keymap.set('n', '<c-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader><leader>', builtin.oldfiles, {})
-vim.keymap.set('n', '<leader>fG', builtin.git_files, {})
+vim.keymap.set('n', '<c-p>', builtin.find_files, { desc = 'Find files' })
+vim.keymap.set('n', '<leader><leader>', builtin.oldfiles, { desc = 'Old files' })
+vim.keymap.set('n', '<leader>fG', builtin.git_files, { desc = 'Git files' })
