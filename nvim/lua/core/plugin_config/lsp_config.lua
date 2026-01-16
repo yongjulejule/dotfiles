dcf9a1dd643b67e3d5c1b7019123d70e76d3f240
Yongjun Lee <lyjshow200@gmail.com>
2026-01-17 00:08:22 +0900

    feat: Update lsp config

--- nvim/lua/core/plugin_config/lsp_config.lua
+++ nvim/lua/core/plugin_config/lsp_config.lua
@@ -1,19 +1,15 @@
 require("mason-lspconfig").setup({
-  ensure_installed = { "lua_ls", "typescript-language-server", "eslint", "terraformls" },
+  ensure_installed = { "lua_ls", "terraformls", "ts_ls" },
 })
 
-local lspconfig = require('lspconfig')
+-- 1. 공통 Capabilities 설정 (v0.11 Native 방식)
+-- 모든 서버에 nvim-cmp 기능을 자동으로 적용합니다.
+vim.lsp.config('*', {
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
+})
 
-local lsp_defaults = lspconfig.util.default_config
-
-lsp_defaults.capabilities = vim.tbl_deep_extend(
-  'force',
-  lsp_defaults.capabilities,
-  require('cmp_nvim_lsp').default_capabilities()
-)
-
--- lua
-lspconfig.lua_ls.setup {
+-- 2. Lua 설정 (vim.lsp.config 사용)
+vim.lsp.config('lua_ls', {
   settings = {
     Lua = {
       diagnostics = {
@@ -27,9 +23,18 @@ lspconfig.lua_ls.setup {
       },
     },
   }
-}
+})
 
--- add lsp keymaps after attaching to a buffer
+-- 3. 서버 활성화 (setup() 대신 enable() 사용)
+vim.lsp.enable('lua_ls')
+vim.lsp.enable('gleam')
+vim.lsp.enable('terraformls')
+-- 필요 시 아래 주석 해제 (ensure_installed에 있는 것들)
+-- vim.lsp.enable('ts_ls')
+-- vim.lsp.enable('eslint')
+
+
+-- 4. Keymaps (LspAttach 이벤트 사용 - 기존 유지)
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(ev)
@@ -37,26 +42,20 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
     -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = ev.buf, desc = 'Go to declaration' })
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = ev.buf, desc = 'Go to definition' })
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = ev.buf, desc = 'Go to References' })
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { buffer = ev.buf, desc = 'Go to implementation' })
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = ev.buf, desc = 'Show LSP hover' })
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, { buffer = ev.buf, desc = 'Add workspace folder' })
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = 'Go to declaration', buffer = ev.buf })
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to definition', buffer = ev.buf })
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = 'Go to References', buffer = ev.buf })
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = 'Go to implementation', buffer = ev.buf })
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Show LSP hover', buffer = ev.buf })
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, { desc = 'Add workspace folder', buffer = ev.buf })
     vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder,
-      { buffer = ev.buf, desc = 'Remove workspace folder' })
+      { desc = 'Remove workspace folder', buffer = ev.buf })
     vim.keymap.set('n', '<space>wl', function()
       print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, { buffer = ev.buf, desc = 'List workspace folders' })
-    vim.keymap.set('n', '<space>gtd', vim.lsp.buf.type_definition, { buffer = ev.buf, desc = 'Go to type definition' })
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, { buffer = ev.buf, desc = 'LSP Refactor: Rename' })
-    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, { buffer = ev.buf, desc = 'LSP Code Action' })
+    end, { desc = 'List workspace folders', buffer = ev.buf })
+    vim.keymap.set('n', '<space>gtd', vim.lsp.buf.type_definition, { desc = 'Go to type definition', buffer = ev.buf })
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, { desc = 'LSP Refactor: Rename', buffer = ev.buf })
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, { desc = 'LSP Code Action', buffer = ev.buf })
   end,
 })
-
--- gleam
-lspconfig.gleam.setup({})
-
--- terraform
-lspconfig.terraformls.setup({})
